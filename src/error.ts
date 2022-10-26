5337eaf40a0668038450842e6534a2ee0cdde235
Marcus Pöhls <mp@seda.digital>
2022-10-26 09:25:32 +0200

    support error cause

--- src/error.ts
+++ src/error.ts
@@ -2,16 +2,6 @@
 
 export class BaseError extends Error {
   /**
-   * Stores the error message.
-   */
-  public override message: string
-
-  /**
-   * Stores the error class name.
-   */
-  public override name: string
-
-  /**
    * Stores the error stacktrace.
    */
   public override stack: string
@@ -22,13 +12,13 @@ export class BaseError extends Error {
    * @param message
    * @param status
    */
-  constructor (message?: string) {
-    super(message)
+  constructor (message?: string, options?: { cause?: any }) {
+    // @ts-expect-error
+    super(message, options)
 
-    this.message = message ?? ''
     this.name = this.constructor.name
 
     this.stack = '' // next line overrides this with the actual stack trace
-    Error.captureStackTrace(this, this.constructor)
+    BaseError.captureStackTrace(this, this.constructor)
   }
 }
